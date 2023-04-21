09ffc65af3a6429cb9be5c7fc65de31cfaed104c
twoosky <lhjae3@naver.com>
2023-04-22 03:56:15 +0900

    init: 프로젝트 세팅

---
+++ src/main/kotlin/com/yourssu/igotIt/IgotItApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.igotIt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IgotItApplication
+
+fun main(args: Array<String>) {
+	runApplication<IgotItApplication>(*args)
+}
---
+++ src/test/kotlin/com/yourssu/igotIt/IgotItApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourssu.igotIt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IgotItApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
