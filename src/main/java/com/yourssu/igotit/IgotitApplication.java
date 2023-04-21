64e682facb79542c133a37946c7dc3d511b62b8f
twoosky <lhjae3@naver.com>
2023-04-22 02:32:07 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/yourssu/igotit/IgotitApplication.java
@@ -0,0 +1,13 @@
+package com.yourssu.igotit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgotitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgotitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourssu/igotit/IgotitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourssu.igotit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgotitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
