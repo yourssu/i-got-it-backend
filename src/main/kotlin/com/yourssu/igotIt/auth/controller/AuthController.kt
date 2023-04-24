19486876bb62ce63063474ccb12c9261840bbe70
twoosky <lhjae3@naver.com>
2023-04-24 23:36:20 +0900

    feat: 소셜로그인 및 닉네임 설정 api 작성

---
+++ src/main/kotlin/com/yourssu/igotIt/auth/controller/AuthController.kt
@@ -0,0 +1,37 @@
+package com.yourssu.igotIt.auth.controller
+
+import com.yourssu.igotIt.auth.dto.LoginInfoRequestDto
+import com.yourssu.igotIt.auth.dto.LoginResponseDto
+import com.yourssu.igotIt.auth.infra.kakao.KakaoLoginRequest
+import com.yourssu.igotIt.auth.service.AuthService
+import com.yourssu.igotIt.user.domain.User
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthController(
+    private val authService: AuthService
+) {
+
+    @PostMapping("/sign-in/kakao")
+    fun signInKakao(
+        @RequestBody request: KakaoLoginRequest
+    ): ResponseEntity<LoginResponseDto> {
+        return ResponseEntity.ok(authService.login(request))
+    }
+
+    @PostMapping("/sign-in/info")
+    fun signInInfo(
+        @Validated @RequestBody request: LoginInfoRequestDto,
+        @AuthenticationPrincipal user: User
+    ): ResponseEntity.BodyBuilder {
+        authService.updateInfo(request, user)
+        return ResponseEntity.ok()
+    }
+}
