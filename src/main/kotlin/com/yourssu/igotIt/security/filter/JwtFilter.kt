109f2386e51a626c5adfd1ff0acbd572d6643aaa
twoosky <lhjae3@naver.com>
2023-04-24 23:29:05 +0900

    feat: jwt filter 작성

---
+++ src/main/kotlin/com/yourssu/igotIt/security/filter/JwtFilter.kt
@@ -0,0 +1,30 @@
+package com.yourssu.igotIt.security.filter
+
+import com.yourssu.igotIt.security.jwt.JwtProvider
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+private const val AUTHORIZATION_HEADER = "Authorization"
+
+class JwtFilter(
+    private val jwtProvider: JwtProvider
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = request.getHeader(AUTHORIZATION_HEADER)
+            ?: throw RuntimeException("Authorization Header is missing")
+        val authentication = jwtProvider.authenticate(token)
+
+        SecurityContextHolder.getContext().apply {
+            this.authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+}
