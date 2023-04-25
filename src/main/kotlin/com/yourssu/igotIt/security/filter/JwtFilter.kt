ade6644488d96e74dd9d26fd291e6257726c0fa9
twoosky <lhjae3@naver.com>
2023-04-25 20:01:30 +0900

    fix: Jwt 헤더 Bearer 스키마 추가

--- src/main/kotlin/com/yourssu/igotIt/security/filter/JwtFilter.kt
+++ src/main/kotlin/com/yourssu/igotIt/security/filter/JwtFilter.kt
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 private const val AUTHORIZATION_HEADER = "Authorization"
+private const val AUTHORIZATION_SCHEMA = "Bearer"
 
 class JwtFilter(
     private val jwtProvider: JwtProvider
@@ -18,14 +19,26 @@ class JwtFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val token = request.getHeader(AUTHORIZATION_HEADER)
+        val authorizationHeader = request.getHeader(AUTHORIZATION_HEADER)
             ?: throw RuntimeException("Authorization Header is missing")
+        val token = extractAccessTokenFromHeader(authorizationHeader)
         val authentication = jwtProvider.authenticate(token)
 
         SecurityContextHolder.getContext().apply {
-            setAuthentication(authentication)
+            this.authentication = authentication
         }
 
         filterChain.doFilter(request, response)
+    }
+
+    private fun extractAccessTokenFromHeader(authorizationHeader: String): String {
+        val splits = authorizationHeader.split(" ")
+        validateAccessToken(splits)
+        return splits[1]
+    }
+
+    private fun validateAccessToken(splits: List<String>) {
+        if (splits.size != 2) throw RuntimeException("잘못된 형식의 Authorization 헤더값 입니다.")
+        if (splits[0] != AUTHORIZATION_SCHEMA) throw RuntimeException("잘못된 Authorization 스키마 입니다.")
     }
 }
