98466837a2ccff01673f850c6fafcf28ad61bcd4
twoosky <lhjae3@naver.com>
2023-04-28 18:32:04 +0900

    feat: 유저에 대한 resolution 조회 쿼리 메서드 작성

--- src/main/kotlin/com/yourssu/igotIt/resolution/domain/ResolutionRepository.kt
+++ src/main/kotlin/com/yourssu/igotIt/resolution/domain/ResolutionRepository.kt
@@ -1,6 +1,8 @@
 package com.yourssu.igotIt.resolution.domain
 
+import com.yourssu.igotIt.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ResolutionRepository : JpaRepository<Resolution, Long> {
+    fun findByUser(user: User): Resolution?
 }
