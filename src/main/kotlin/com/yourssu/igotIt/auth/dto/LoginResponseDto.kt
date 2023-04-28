c1983905e2641c325746bf2c8b75b39cde15fee9
twoosky <lhjae3@naver.com>
2023-04-28 18:31:07 +0900

    feat: 로그인 response에 resolutionId 추가

--- src/main/kotlin/com/yourssu/igotIt/auth/dto/LoginResponseDto.kt
+++ src/main/kotlin/com/yourssu/igotIt/auth/dto/LoginResponseDto.kt
@@ -2,7 +2,7 @@ package com.yourssu.igotIt.auth.dto
 
 data class LoginResponseDto(
     val userId: Long,
-    val isNewUser: Boolean,
     val nickname: String?,
+    val resolutionId: Long?,
     val accessToken: String
 )
