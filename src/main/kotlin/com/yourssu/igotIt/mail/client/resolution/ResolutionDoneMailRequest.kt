585278a26aaa1c71db1d2679b0e9c54bb20cfe4b
twoosky <lhjae3@naver.com>
2023-04-29 17:10:50 +0900

    feat: 결심 생성 링크 생성 메서드 작성

--- src/main/kotlin/com/yourssu/igotIt/mail/client/resolution/ResolutionDoneMailRequest.kt
+++ src/main/kotlin/com/yourssu/igotIt/mail/client/resolution/ResolutionDoneMailRequest.kt
@@ -11,7 +11,7 @@ private const val type = "email"
 data class ResolutionDoneMailRequest(
     val email: String,
     val resolution: Resolution,
-    val user: User,
+    val user: User
 ) : MailRequest {
 
     override fun email(): String = email
@@ -24,14 +24,18 @@ data class ResolutionDoneMailRequest(
      * 나에게 편지 쓴 내용: 은 현재 구현 불가
      * -> letter는 userId를 갖고 있지 않으므로, resolutionId로 해당 결심의 쪽지를 찾아도 자신이 쓴 쪽지를 찾을 수 없음
      * 사용자 이름: resolution.user.nickname
-     * TODO: resolution에 link 컬럼 저장 후 불러오기
+     * 결심 링크
      */
-    override fun createContext(): Context {
+    override fun createContext(baseUrl: String): Context {
         return Context().apply {
             setVariable("period", resolution.period)
             setVariable("content", resolution.content)
             setVariable("nickname", user.nickname)
-            setVariable("link", "https://letter.yourssu.com/")
+            setVariable("link", generateLink(baseUrl))
         }
+    }
+
+    private fun generateLink(baseUrl: String): String {
+        return "${baseUrl}/resolutions/${resolution.id}"
     }
 }
