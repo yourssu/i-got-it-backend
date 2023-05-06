852484d4fe3c86a3df198fbe6246693c63fdf969
twoosky <lhjae3@naver.com>
2023-05-07 00:14:05 +0900

    feat: 결심에 대한 쪽지 삭제 쿼리 메서드 추가

---
+++ src/main/kotlin/com/yourssu/igotIt/auth/dto/AccessTokenRefreshResponse.kt
@@ -0,0 +1,5 @@
+package com.yourssu.igotIt.auth.dto
+
+class AccessTokenRefreshResponse(
+    val accessToken: String
+)
--- src/main/kotlin/com/yourssu/igotIt/letter/domain/LetterRepository.kt
+++ src/main/kotlin/com/yourssu/igotIt/letter/domain/LetterRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface LetterRepository : JpaRepository<Letter, Long> {
     fun findAllByResolution(resolution: Resolution): List<Letter>
+    fun deleteAllByResolution(resolution: Resolution)
 }
